301a12a0a2c64c5d8ef0d9b35b7ac3e438cb5929
AlessandraLopesFerreyra <159266652+AlessandraLopesFerreyra@users.noreply.github.com>
2025-02-26 08:41:50 -0300

    Add files via upload

---
+++ ale/10ate1.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+   int n=10;
+   while(n<=10 && n>=1){
+       printf(" %d", n);
+       n=n-1;
+   }
+    return 0;
+}
